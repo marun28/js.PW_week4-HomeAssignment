d2d1b8d84fb10c5dffbe376479886023d5ca0d91
marun28 <83106497+marun28@users.noreply.github.com>
2025-09-08 22:20:40 +0530

    Add files via upload

---
+++ week4-hA1.spec.ts
@@ -0,0 +1,22 @@
+import test from '@playwright/test'
+//promt alert
+test("Handle Alert with page.on",async({page})=>{
+
+
+//handle first alert present in the test case
+page.once('dialog',alertType=>{
+// type of alert
+const type=alertType.type()
+console.log(type)
+//alert box what message it has 
+console.log(alertType.message())
+//handle alert 
+alertType.dismiss()
+
+})
+
+await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")
+await page.frameLocator("//iframe[@id='iframeResult']").locator("//button[text()='Try it']").click()
+const mess = await page.frameLocator("//iframe[@id='iframeResult']").locator(`//p[@id='demo']`).innerText()
+console.log(mess)
+})
